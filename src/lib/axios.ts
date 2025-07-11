2271298f6a15bbc2033e01706ed92c691e82a81f
AlexJosua <130466775+AlexJosua@users.noreply.github.com>
2025-07-11 21:16:47 +0700

    setup vercel

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // ganti sesuai backend kamu
+  baseURL: import.meta.env.VITE_API_URL, // ganti sesuai backend kamu
 });
 
 // Tambahkan token secara otomatis jika ada
